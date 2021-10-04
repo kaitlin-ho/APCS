85e813858815767f072fba34ad9aa3174ad34060
kaitlin-ho <kho30@stuy.edu>
2021-10-04 19:21:29 -0400

    hw10

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,22 @@
+Kaitlin Ho
+APCS
+HW10 - Refactor BigSib One
+2021-10-04
+
+/*
+DISCOVERIES
+- if you have a class that isn't void, you need to return something
+
+UNRESOLVED QUESTIONS
+- why is static needed?
+
+*/
+
+
+
+public class BigSib {
+  public static String greet(String n){
+    String s = "Why, hello, there, " + n + ". How do you do?";
+    return s;
+  }
+}
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,12 @@
+Kaitlin Ho
+APCS
+HW10 - Refactor Big Sib One
+2021-10-04
+
+public class Greet {
+    public static void main(String[] args){
+        System.out.println(BigSib.greet("Crosby"));
+        System.out.println(BigSib.greet("Stills"));
+        System.out.println(BigSib.greet("Nash"));
+    }
+}
