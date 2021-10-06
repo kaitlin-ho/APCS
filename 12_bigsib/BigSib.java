06e511460ea25fb4df94fffc4668be0bab99b4d2
kaitlin-ho <kho30@stuy.edu>
2021-10-05 21:16:36 -0400

    hw12

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -6,10 +6,28 @@ Worked with Ariella, Ziying
 
 DISCOVERIES
 - System.out.print works with void
+- you cannot use instanced variables with static
 
 UNRESOLVED QUESTIONS
 - What is an instanced variable?(actual definition)
+- How do you use an instanced variable helloMsg AND print the rest of the message? e.g. after Word Up Freshmen
 
+My partner's code: 
+public class BigSib{
+ String helloMsg;
+ 
+  public String greet(String name){
+    return(helloMsg + " " + name);
+  }
+ 
+  public void setHelloMsg(String msg){
+   helloMsg = msg;
+  }
+ 
+}
+
+The above uses the instanced variable and works, but I couldn't seem to add the rest of the message onto it, because 
+of the type (void, String). The below is what I did to get the desired output, but does not use the instanced variable.
 
 */
 
@@ -26,3 +44,5 @@ public class BigSib {
 
 
 }
+
+
