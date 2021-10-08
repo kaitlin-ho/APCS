be28a0ebe15529e6521cffe5e1ac0eb1ded395e0
kaitlin-ho <kho30@stuy.edu>
2021-10-07 20:49:23 -0400

    hw14

--- 14_overload/defcon/Greet.java
+++ 14_overload/defcon/Greet.java
@@ -1,3 +1,19 @@
+//BARK: Brian, Apple (ducky), Ryan, Kaitlin; Duckies: Jon, Vivian 
+//APCS
+//HW14 - Customize Your Creation
+//2021-10-07
+
+/*
+DISCO
+- Things in the default constructor can be overwritten 
+- static before a variable means that it is shared across all instances of the class
+	e.g. static String helloMsg would mean that the last new BigSib instance created (in this case it was "Salutations") would be used as the helloMsg for all 4 BigSibs 
+
+QCC
+- What is the purpose of making a method static versus making it nonstatic?
+
+*/
+
 public class Greet {
     public static void main(String[] args) {
         String greeting;
