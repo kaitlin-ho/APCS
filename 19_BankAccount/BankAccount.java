224aadd96f839dc8952a0c608d51720caaa17ebb
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-10-18 19:46:35 -0400

    Update BankAccount.java

--- 19_BankAccount/BankAccount.java
+++ 19_BankAccount/BankAccount.java
@@ -1,48 +1,96 @@
-public class BankAccount {
-  public String holderName;
-  private String accountPassword;
-  private int pin;
-  public int accountNumber;
-  private double accBalance;
+/**
+   NIWWD: Kevin Xiao + Mr. Swag, Jeffery Tang + Mathias, Kaitlin Ho + Apple 
+   APCS
+   HW19 -- Better BankAccount in Java
+   2021-10-19
+**/
 
-  private void withdraw (double withdrawAmount) {
-    accBalance = accBalance - withdrawAmount;
-    }
-  private void deposit (double depositAmount) {
-    accBalance = accBalance + depositAmount;
-    }
-  private String setHolderName (String name) {
-    holderName = name;
-    return holderName;
-    }
-  private int setAccountNumber (int accNum) {
-    accountNumber = accNum;
-    return accountNumber;
-    }
-  private String setAccountPassword (String accPW) {
-    accountPassword = accPW;
-    return accountPassword;
-    }
-  private int setPin (int accPIN) {
-    pin = accPIN;
-    return pin;
-    }
-  public double setBalance(double newBalance ) {
-     accBalance = newBalance;
-    return accBalance;
+/**
+   DISCO: 
+   QCC: 
+**/
+
+public class BankAccount {
+
+  //instance variables
+  private String name;
+  private String passwd;
+  private short pin;
+  private int acctNum;
+  private double balance;
+  //---------------------------------------------
+
+
+  //mutators
+  public String setName( String newName ) {
+    String oldName = name;
+    name = newName;
+    return oldName;
   }
-  public String toString () {
-    return("" + holderName + "\n" + accountNumber + "\n" + accBalance);
+
+  public String setPasswd( String newPasswd ) {
+    String oldPasswd = passwd;
+    passwd = newPasswd;
+    return oldPasswd;
+  }
+
+  public short setPin( short newPin ) {
+    short oldPin = pin;
+    pin = newPin;
+    return oldPin;
+  }
+
+  public int setAcctNum( int newAcctNum ) {
+    if (100000000 <= newAcctNum & newAcctNum <= 999999998) {
+      int oldAcctNum = acctNum;
+      acctNum = newAcctNum;
+      return oldAcctNum;
     }
-  public static void main(String[] args) {
-    BankAccount Account = new BankAccount ();
-    Account.setHolderName("John");
-    Account.setAccountPassword("hellotheremyfriends");
-    Account.setPin(2131);
-    Account.setAccountNumber(123456789);
-    Account.setBalance(1000.00);
-    Account.deposit(500);
-    Account.withdraw(241);
-    System.out.println(Account.toString());
-    }
+    else {
+       System.out.println ("The entered Account Number is not a 9 digit number; please enter a 9 digit number");
+  }
+
+  public double setBalance( double newBalance ) {
+    double oldBalance = balance;
+    balance = newBalance;
+    return oldBalance;
+  }
+  //---------------------------------------------
+
+
+  public void deposit( double depositAmount ) {
+    balance = balance + depositAmount;
+  }
+
+  public void withdraw( double withdrawAmount ) {
+    balance = balance - withdrawAmount;
+  }
+
+
+  //overwritten toString()
+  public String toString() {
+    String retStr = "\nAccount info:\n=======================";
+    retStr = retStr + "\nName: " + name;
+    retStr = retStr + "\nPassword: " + passwd;
+    retStr = retStr + "\nPIN: " + pin;
+    retStr = retStr + "\nAccount Number: " + acctNum;
+    retStr = retStr + "\nBalance: " + balance;
+    retStr = retStr + "\n=======================";
+    return retStr;
+  }
+
+
+  //main method for testing
+  public static void main( String[] args ) {
+    BankAccount ba = new BankAccount();
+    ba.setName("John");
+    ba.setPasswd("hellotheremyfriends");
+    ba.setPin((short) 2131);
+    ba.setAcctNum(123456789);
+    ba.setBalance(1000.00);
+    ba.deposit(500.0);
+    ba.withdraw(241.0);
+    System.out.println(ba.toString());
+  }
+
 }
