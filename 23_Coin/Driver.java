078c4ae500f05b68936088cf4c0ff6d84bffd7e2
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-10-24 18:54:17 -0400

    Update Driver.java

--- 23_Coin/Driver.java
+++ 23_Coin/Driver.java
@@ -27,13 +27,13 @@ public class Driver {
       System.out.println("mine: " + mine);
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
-      /*===================TOP==========================
       //test flip() method
       System.out.println("\nAfter flipping...");
       yours.flip();
       wayne.flip();
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
+      /*===================TOP==========================
       //test equals() method
       if ( yours.equals(wayne) ) {
         System.out.println( "Matchee matchee!" );
