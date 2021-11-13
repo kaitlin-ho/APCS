9171c7a6ab2289897c5bdee762ad290028b601bb
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-11-13 18:31:55 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -88,10 +88,12 @@ static int[] test2 = {1,2,3,30, 2140, 222, 124, 1, 1, 24, 30, 1};
 	
 	public static void main(String[] args){
 		System.out.println(sVer(popInt(test)));
-		System.out.println(linSearch(test2, 1));
-		System.out.println(freq(test2, 1));
+		System.out.println(linSearch(test2, 1)); // should be 0
+		System.out.println(freq(test2, 1)); //should be 4
 		System.out.println(linSearchR(test2, 4)); // should be -1
       		System.out.println(linSearchR(test2,3)); // should be 2
+		System.out.println(freqR(test2, 1)); // should be 4
+		System.out.println(freqR(test2, 24100)); // should be -1
 	}
 
 }
