7830f9df5a2d1ff5273f6d21696c359e69cf458e
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-11-13 16:32:48 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -5,17 +5,17 @@ static int[] test = new int [10];
 static int[] test2 = {1,2,3,30};
 	
 	//A static method to populate an existing array with randomly generated ints
-	public static int[] popInt(int[] A) {
-		for(int i = 0; i < A.length; i++){
-			A[i] = (int)(Math.random()*100);
+	public static int[] popInt(int[] a) {
+		for(int i = 0; i < a.length; i++){
+			a[i] = (int)(Math.random()*100);
 		}
-		return A;
+		return a;
 	}		
 	//A static method to return a String version of an array of ints
-	public static String sVer(int[] A) {
+	public static String sVer(int[] a) {
 		String result = "";
-		for(int i = 0; i < A.length; i++){
-			result = result + A[i] + ", ";
+		for(int i = 0; i < a.length; i++){
+			result = result + a[i] + ", ";
 		}
 		return result.substring(0,result.length()-1);
 	}
@@ -24,13 +24,27 @@ static int[] test2 = {1,2,3,30};
 	//the index of the first occurrence of a target in an existing array, or -1 if not found
 	
 	//Iterative
-	public static int linSearch(int[] A, int target){
-		for(int i = 0; i < A.length; i++){
-			if (A[i] == target){
+	public static int linSearch(int[] a, int target){
+		for(int i = 0; i < a.length; i++){
+			if (a[i] == target){
 				return i;
 			}
 		}
 		return -1;
+	}
+	
+	//public, static implementations of a frequency function that will return the number of 
+	//occurrences of a target in an existing array
+	
+	//Iterative
+	public static int freq(int[] a, int target){
+		int freqCount;
+		for(int i = 0; i < a.length; i++){
+			if (a[i] == target){
+				freqCount += 1;
+			}
+		}
+		return freqCount;
 	}
 	
 	public static void main(String[] args){
