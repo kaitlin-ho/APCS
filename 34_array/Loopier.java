4552a30183df7fa50d60e2d61b7280e0823f1a90
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-11-13 18:43:53 -0500

    Update Loopier.java

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -2,7 +2,7 @@ public class Loopier {
 
 
 static int[] test = new int [10];
-static int[] test2 = {1,2,3,30, 2140, 222, 124, 1, 1, 24, 30, 1};
+static int[] test2 = {1,2,3,30, 2140, 222, 124, 1, 1, 24, 30, 1, 3};
 	
 	//A static method to populate an existing array with randomly generated ints
 	public static int[] popInt(int[] a) {
@@ -17,7 +17,7 @@ static int[] test2 = {1,2,3,30, 2140, 222, 124, 1, 1, 24, 30, 1};
 		for(int i = 0; i < a.length; i++){
 			result = result + a[i] + ", ";
 		}
-		return result.substring(0,result.length()-1);
+		return result.substring(0,result.length()-2);
 	}
 	
 	//public static implementations of a linear search function that will return 
@@ -71,29 +71,25 @@ static int[] test2 = {1,2,3,30, 2140, 222, 124, 1, 1, 24, 30, 1};
 	
 	//Recursive
 	public static int freqR(int[] a, int target){
-		if (a.length == 0){
-			return -1;
+		if (a[0] == target) {return 1;}
+
+		int[] aNew = new int[a.length-1];
+		
+		for (int i = 1; i < a.length; i++){
+			aNew[i-1] = a[i];
 		}
-		else{
-			if (a[0] == target) {return 1;}
-			else {
-				int[] aNew = new int[a.length-1];
-				for (int i = 1; i < a.length; i++){
-					aNew[i-1] = a[i];
-				}
-				return 1 + freqR(aNew, target);
-			}
-		}
+		
+		return 1 + freqR(aNew, target);
 	}
 	
 	public static void main(String[] args){
-		System.out.println(sVer(popInt(test)));
+		// System.out.println(sVer(popInt(test)));
 		System.out.println(linSearch(test2, 1)); // should be 0
 		System.out.println(freq(test2, 1)); //should be 4
-		System.out.println(linSearchR(test2, 4)); // should be -1
+		// System.out.println(linSearchR(test2, 4)); // should be -1 because 4 is not in the array
       		System.out.println(linSearchR(test2,3)); // should be 2
 		System.out.println(freqR(test2, 1)); // should be 4
-		System.out.println(freqR(test2, 24100)); // should be -1
+		System.out.println(freqR(test2, 24100)); // should be -1 because it is not in the array
 	}
 
 }
