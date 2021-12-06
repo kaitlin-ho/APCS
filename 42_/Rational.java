a6ed1ee2ddf3ab40261ecdacf879ab8bc5b228f5
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-12-05 20:58:55 -0500

    Update Rational.java

--- 42_/Rational.java
+++ 42_/Rational.java
@@ -67,14 +67,14 @@ public class Rational {
 
   public void add(Rational a){
      int leastCom = lcm(this.q, a.q);
-     p = p * leastCom + (a.p)*leastCom;
-     q = q * leastCom;
+     p = (leastCom/q)*this.p - (leastCom/a.q)*a.p;
+     q = this.q * leastCom;
   }
 
   public void subtract(Rational a){
      int leastCom = lcm(this.q, a.q);
-     p = p * leastCom - (a.p)*leastCom;
-     q = q * leastCom;
+     p = (leastCom/q)*this.p - (leastCom/a.q)*a.p;
+     q = this.q * leastCom;
   }
 	
   public int lcm(int a, int b){
