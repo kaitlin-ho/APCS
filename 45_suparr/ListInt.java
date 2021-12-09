018e3996a8cbe63407d93e6db713a7ed46b9fcfb
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-12-08 22:08:08 -0500

    Update ListInt.java

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -1,7 +1,7 @@
 public interface ListInt
 {
   
-  public boolean add(int newVal);
+  public void add(int newVal);
   public void add(int index, int newVal);
   public void remove(int index);
   public int size();
