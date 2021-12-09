97977f4401c9eeab49dc9aca3db51dd44b8568bb
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-12-08 22:09:59 -0500

    Update ListInt.java

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -5,7 +5,7 @@ public interface ListInt
   public void add(int index, int newVal);
   public void remove(int index);
   public int size();
-  public int get(int index);
-  //public int set(int index);
+  //public int get(int index); // this works
+  //public int set(int index); // this does not work, Error: SuperArray is not abstract and does not override abstract method set(int) in ListInt
   
 }
