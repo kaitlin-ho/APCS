031b14741686f09db66a2e49043c707760f16c4b
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-12-22 22:43:13 -0500

    Update Rational.java

--- 49_rat/Rational.java
+++ 49_rat/Rational.java
@@ -105,18 +105,5 @@ public class Rational {
 		return false;
 	}
 
-	public static void main(String[] args) {
-		Rational r = new Rational(2,3);
-		Rational t = new Rational(4,6);
-		Rational s = new Rational(1,2);
-		Object notRat = new Object();
-
-		System.out.println(r.equals(r)); // should be true
-		System.out.println(r.equals(t)); // should be true
-		System.out.println(r.equals(s)); // should be false
-		System.out.println(r.equals(notRat)); // should be false
-
-
-	}
 
 }
