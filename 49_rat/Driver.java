6f784f2bae0155558db88da8ae7e2c418d103f56
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2021-12-22 22:42:57 -0500

    Update Driver.java

--- 49_rat/Driver.java
+++ 49_rat/Driver.java
@@ -1,1 +1,16 @@
+public class Driver{
+  
+public static void main(String[] args) {
+		Rational r = new Rational(2,3);
+		Rational t = new Rational(4,6);
+		Rational s = new Rational(1,2);
+		Object notRat = new Object();
 
+		System.out.println(r.equals(r)); // should be true
+		System.out.println(r.equals(t)); // should be true
+		System.out.println(r.equals(s)); // should be false
+		System.out.println(r.equals(notRat)); // should be false
+
+
+	}
+}
