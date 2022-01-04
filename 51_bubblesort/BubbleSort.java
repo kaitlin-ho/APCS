b5eb274a89b95303230dd403500000b9d9ae6809
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2022-01-03 22:14:36 -0500

    Update BubbleSort.java

--- 51_bubblesort/BubbleSort.java
+++ 51_bubblesort/BubbleSort.java
@@ -67,16 +67,16 @@ public class BubbleSort
   // ArrayList-returning bubbleSort
   // postcondition: order of input ArrayList's elements unchanged
   //                Returns sorted copy of input ArrayList.
-  public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input )
-  {
-    /* YOUR IMPLEMENTATION HERE */
-  }
+//   public static ArrayList<Comparable> bubbleSort( ArrayList<Comparable> input )
+//   {
+//     /* YOUR IMPLEMENTATION HERE */
+//   }
 
 
   public static void main( String [] args )
   {
 
-    /*===============for VOID methods=============
+    
       ArrayList glen = new ArrayList<Integer>();
       glen.add(7);
       glen.add(1);
@@ -84,6 +84,8 @@ public class BubbleSort
       glen.add(12);
       glen.add(3);
       System.out.println( "ArrayList glen before sorting:\n" + glen );
+      
+      /*===============for VOID methods=============
       bubbleSortV(glen);
       System.out.println( "ArrayList glen after sorting:\n" + glen );
       ArrayList coco = populate( 10, 1, 1000 );
