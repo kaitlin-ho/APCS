b28c9d921b3de8782a821146df4958131db54eae
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2022-01-12 21:42:16 -0500

    Update Magpie5.java

--- cobo/magpie/activity5/Magpie5.java
+++ cobo/magpie/activity5/Magpie5.java
@@ -1,3 +1,18 @@
+/*
+Erica's Fans and Hugo
+APCS
+HW56 - Turing Training Wheels
+2022-1-12
+time spent: 2
+
+QCC:
+- In activty4 there was an out of bounds issue with the I You statement and response. In line 67, removing the space from "I " (changed to "I") solved the out of bounds issue. Why is this? How could a single space cause this error.
+
+DISCO:
+- Learned how to open html files in java and in terminal
+- User input obtained from the scanner can easily be manipulated (i.e using methods like an overridden indexOf, transform methods using substring, etc)
+*/
+
 import java.util.Random;
 
 /**
