2d5e0afd28fa9d755a059c91d7944198687a8707
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2022-03-16 07:37:08 -0400

    Update LList.java

--- 77_llist/LList.java
+++ 77_llist/LList.java
@@ -1,22 +1,32 @@
-
 /**
  * Mr. K: May Qiu, Kaitlin Ho, Raven (Ruiwen) Tang
  * APCS pd06
- * HW76 -- We Got a Little Ol' Convoy / LList implementation
- * 2022-03-14
- * time spent: 1.0 hr
- * KtS consumed: 2
- *
+ * HW77 -- Insert|Remove
+ * 2022-03-15
+ * time spent: 1.5 hr
+ * KTS used: 1
  * DISCO
- * Since LList implements the interface List, every method in the List interface has to be uncommented in LList. So instead of commenting out incomplete methods like we usually do, we can get around this by returning fake values to allow the methods to compile.
- * One can't set the cargo of a null node. So, when we're traversing, we need to check if temp.getNext() is null instead of if temp is null.
- * A lot of methods that are O(1) for arrays and ArrayLists, like set(), get(), size(), and add() [in many cases], take O(n) time for LinkedLists, because you're doing getNext() until you hit the end of the list or the desired index.
+ * We realized that we needed to split remove() into a few cases, because the desired index of removal might not point to a non-null node. In this case,
+ * we want to set the node before the desired index to point to null. Additionally, if we want to remove the first index, there won’t be a node before it,
+ * so we want to change where _head points.
+ *
+ * add() actually adds an element to the beginning of the list, rather than the end (which is what we thought in HW76). This means it should take O(1) time
+ * since it is only changing _head, instead of O(n) time.
+ *
+ * In other Lists, we usually have to shift all the elements after the removed element back one index. However, for LinkedList remove(), we don’t even need
+ * to look at the later elements. We only need to change the pointer of the element before the remove index.
  *
  * QCC
- * We are still a bit confused about using temp as a pointer. When we set the next node of temp, we are able to also set the last node of the list beginning with _head, but we're still unsure of how this actually works. We also haven't really seen this with other variables and objects.
- * When you work on a new class, it's important to finish toString() before working on other methods, because it allows you to test if your other methods are correct.
- * You can traverse a LinkedList by a while loop that checks if nodes are null OR a for loop that counts elements until the size of the list is hit.
- * What is the benefit of using a LinkedList, especially because a lot of methods take longer runtime than ArrayLists.
+ * Would it be reasonable to also make a add at index method?
+ *
+ * ALGO ADD
+ * Create a new node temp with cargo newVal and link it to _head. Then, let _head point to temp. Return true.
+ *
+ * ALGO REMOVE
+ * If index is out of bounds, throw IndexOutOfBoundsException.
+ * If index is 0, store the value at head and change the head to the next node. Return stored value.
+ * If _size - index is less than two, create a temp pointing to head. Move temp until it is at index - 1. Store temp's next node's value. Set temp's next node to null. Return the stored value.
+ * Else, create a temp pointing to head. Move temp until it is at index - 1. Store temp's next node's value. Set temp's next node to the node 2 indices from temp. Return the stored value.
  **/
 
 /***
@@ -65,7 +75,7 @@ public class LList implements List //interface def must be in this dir
     LLNode temp = new LLNode(newVal, _head);
     _head = temp;
     _size += 1;
-
+    return true;
   }
 
 
@@ -107,15 +117,28 @@ public class LList implements List //interface def must be in this dir
 
   //remove at index
   public String remove(int index){
+    if ( index < 0 || index >= size() ) throw new IndexOutOfBoundsException();
+    if(index == 0){
+      String ans = _head.getCargo();
+      _head = _head.getNext();
+      return ans;
+  }
+    if((_size-index)<2){
+      LLNode temp = _head;
+      for(int i = 0; i < index - 1; i++){
+          temp = temp.getNext();
+      }
+      String ans = temp.getNext().getCargo();
+      temp.setNext(null);
+      return ans;
+    }
     LLNode temp = _head;
     for (int i = 0; i < index - 1; i++){
       temp = temp.getNext();
     }
     temp.setNext(temp.getNext().getNext());
+    return previous;
   }
-
-
-
   //--------------^  List interface methods  ^--------------
 
 
@@ -147,27 +170,43 @@ public class LList implements List //interface def must be in this dir
     james.add("test1");
     System.out.println( james ); //[test1]
     james.add("test2");
-    System.out.println( james ); //[test1, test2]
+    System.out.println( james ); //[test2, test1]
     james.add("test3");
-    System.out.println( james ); //[test1, test2, test3]
+    System.out.println( james ); //[test3, test2, test1]
     System.out.println( "size: " + james.size() ); //3
     james.add("beat");
-    System.out.println( james ); //[test1, test2, test3, beat]
+    System.out.println( james ); //[beat, test3, test2, test1]
     System.out.println( "size: " + james.size() ); //4
     james.add("a");
-    System.out.println( james ); //[test1, test2, test3, beat, a]
+    System.out.println( james ); //[a, beat, test3, test2, test1]
     System.out.println( "size: " + james.size() ); //5
     james.add("need");
-    System.out.println( james ); //[test1, test2, test3, beat, a, need]
+    System.out.println( james ); //[need, a, beat, test3, test2, test1]
     System.out.println( "size: " + james.size() ); //6
     james.add("I");
-    System.out.println( james ); //[test1, test2, test3, beat, a, need, I]
+    System.out.println( james ); //[I, need, a , beat, test3, test2, test1]
     System.out.println( "size: " + james.size() ); //7
-    System.out.println( "2nd item is: " + james.get(1) ); //test2
+    System.out.println( "2nd item is: " + james.get(1) ); //need
     james.set( 1, "got" );
     System.out.println( "...and now 2nd item is: " + james.set(1,"got") ); //got
-    System.out.println( james ); //[test1, got, test3, beat, a, need, I]
+    System.out.println( james ); //[I, got, a, beat, test3, test2, test1]
+    System.out.println("TIME TO REMOVEEEEEE");
+    james.remove(2);
+    System.out.println(james);
+    james.remove(5);
+    System.out.println(james);
+    james.remove(0);
+    System.out.println(james);
+    james.remove(1);
+    System.out.println(james);
+    james.remove(2);
+    System.out.println(james);
+    james.remove(0);
+    System.out.println(james);
+    james.remove(0);
+    System.out.println(james);
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }
 
