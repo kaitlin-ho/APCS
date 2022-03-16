a64be8e22c32eb604c509527de26cbaf75f731a8
Kaitlin Ho <76082296+kaitlin-ho@users.noreply.github.com>
2022-03-16 07:36:14 -0400

    Update LLNode.java

--- 77_llist/LLNode.java
+++ 77_llist/LLNode.java
@@ -1,22 +1,10 @@
-
 /**
  * Mr. K: May Qiu, Kaitlin Ho, Raven (Ruiwen) Tang
  * APCS pd06
- * HW76 -- We Got a Little Ol' Convoy / LList implementation
+ * HW77 -- INSERT|REMOVE
  * 2022-03-14
- * time spent: 1.0 hr
- * KtS consumed: 2
- *
- * DISCO
- * Since LList implements the interface List, every method in the List interface has to be uncommented in LList. So instead of commenting out incomplete methods like we usually do, we can get around this by returning fake values to allow the methods to compile.
- * One can't set the cargo of a null node. So, when we're traversing, we need to check if temp.getNext() is null instead of if temp is null.
- * A lot of methods that are O(1) for arrays and ArrayLists, like set(), get(), size(), and add() [in many cases], take O(n) time for LinkedLists, because you're doing getNext() until you hit the end of the list or the desired index.
- *
- * QCC
- * We are still a bit confused about using temp as a pointer. When we set the next node of temp, we are able to also set the last node of the list beginning with _head, but we're still unsure of how this actually works. We also haven't really seen this with other variables and objects.
- * When you work on a new class, it's important to finish toString() before working on other methods, because it allows you to test if your other methods are correct.
- * You can traverse a LinkedList by a while loop that checks if nodes are null OR a for loop that counts elements until the size of the list is hit.
- * What is the benefit of using a LinkedList, especially because a lot of methods take longer runtime than ArrayLists.
+ * time spent: 1.5 hr
+ * KtS consumed: 1
  **/
 
 import java.util.ArrayList;
