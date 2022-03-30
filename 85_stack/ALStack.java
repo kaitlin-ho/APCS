e068e274a236f03c744739ea286fb3a1b6fc3d7e
kaitlin-ho <kho30@stuy.edu>
2022-03-30 12:33:57 -0400

    ss

---
+++ 85_stack/ALStack.java
@@ -0,0 +1,31 @@
+public class ALStack implements Stack<PANCAKE> {
+
+  private ArrayList<PANCAKE> foo;
+
+  public ALStack(){
+    foo = new ArrayList<PANCAKE>();
+  }
+
+  public boolean isEmpty(){
+    return (foo.size() == 0);
+  }
+
+  public PANCAKE peekTop(){
+
+
+  }
+
+  public PANCAKE pop(){
+
+
+  }
+
+  public void push( PANCAKE x ){
+
+
+  }
+
+}
+
+
+//ALStack<PANCAKE>
---
+++ 85_stack/LLStack.java
@@ -0,0 +1,3 @@
+public class LLStack implements Stack<PANCAKE>{
+
+}
